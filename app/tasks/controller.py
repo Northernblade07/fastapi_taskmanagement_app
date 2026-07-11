1bbcc1b74646c2f045c106515ad4b2a203f511cb
Prashannt Bhandari <prashantbhandaricr07@gmail.com>
2026-07-11 22:46:11 +0530

    create all task api endpoint including get all , update , delete , create , get_one function

--- app/tasks/controller.py
+++ app/tasks/controller.py
@@ -1,6 +1,7 @@
 from app.tasks.dtos import TaskSchema
 from sqlalchemy.orm import Session
 from app.tasks.models import TaskModel
+from fastapi import HTTPException
 def create_task(body:TaskSchema , db:Session): 
     data = body.model_dump()
     new_task = TaskModel(
@@ -13,3 +14,51 @@ def create_task(body:TaskSchema , db:Session):
     db.commit()
     db.refresh(new_task)
     return {"status":"success" , "message":"task created successfully" , "data":new_task}
+
+
+def get_all_tasks(db:Session):
+    return db.query(TaskModel).all()
+
+
+def get_task( task_id:int, db : Session):
+    task = db.query(TaskModel).get(task_id)
+
+    if not task:
+        raise HTTPException(404 , detail="Task not found")
+    return {
+        "status":"success",
+        "data":task
+    }
+
+
+def update_task(body:TaskSchema , task_id:int , db:Session):
+    task = db.query(TaskModel).get(task_id)
+
+    if not task:
+        raise HTTPException(404 , detail="Task not found")
+    
+    # this below is if you want to update every field manually
+    # task.title = body.title
+    # task.description = body.description
+    # task.is_completed = body.is_completed
+
+    # otherwise use a for loop for updating every field like below
+
+    for field , value in body.model_dump().items():
+        setattr(task , field , value)
+
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    return {"status":"success" , "message":"task updated successfully" , "data":task}
+
+
+def delete_task(task_id:int , db:Session):
+    task = db.query(TaskModel).get(task_id)
+
+    if not task :
+        raise HTTPException(404 , detail="Task not found")
+    
+    db.delete(task)
+    db.commit()
+    return {"status":"success" , "message":"task deleted successfully"}
--- app/tasks/router.py
+++ app/tasks/router.py
@@ -8,3 +8,26 @@ task_routes = APIRouter(prefix="/tasks" , tags=["Tasks"])
 @task_routes.post("/create")
 def create_task(body: TaskSchema , db = Depends(get_db)):
     return controller.create_task(body , db)
+
+@task_routes.get("/all")
+def get_all_tasks(db = Depends(get_db)):
+    tasks =  controller.get_all_tasks(db)
+    return {
+        "status":"success",
+        "data":tasks
+    }
+
+@task_routes.get("/{task_id}")
+def get_tasks( task_id:int, db = Depends(get_db)):
+    task = controller.get_task(task_id, db)
+    return task
+
+
+@task_routes.put("/update/{task_id}")
+def update_task(body:TaskSchema , task_id:int , db = Depends(get_db)):
+    return controller.update_task(body , task_id , db)
+
+
+@task_routes.delete("/delete/{task_id}")
+def delete_task(task_id:int , db = Depends(get_db)):
+    return controller.delete_task(task_id , db)
